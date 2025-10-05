d0c3b0aba72ae4ef9527654e353b5db8faf387fd
Svetlana Tupchina <lena.flores01@mail.ru>
2025-10-05 21:07:47 +0300

    hello message was changed

--- src/main/java/ru/mail/flores01/lena/Main.java
+++ src/main/java/ru/mail/flores01/lena/Main.java
@@ -2,6 +2,6 @@ package ru.mail.flores01.lena;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello qa.guru!");
     }
 }
